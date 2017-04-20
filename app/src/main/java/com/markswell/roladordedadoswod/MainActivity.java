076c17c2c818c5ccb8724d5e1a2a560c472d28b0
markswell <markswellmenezes@outlook.com>
2017-04-19 22:39:57 -0300

    commit inicial somente tela

---
+++ app/src/main/java/com/markswell/roladordedadoswod/MainActivity.java
@@ -0,0 +1,13 @@
+package com.markswell.roladordedadoswod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
